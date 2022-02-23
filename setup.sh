cefbd6560991b4c445da6448ab61a268f5d47b2c
Wibbler67 <76840856+Wibbler67@users.noreply.github.com>
2022-02-23 16:19:49 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 
 # install docker if it isn't already
 if ! docker --version > /dev/null; then
-    curl -V https://get.docker.com | sudo bash
+    curl -V https://get.docker.com | bash
 fi
 
 MYSQL_DATABASE="bookshelve"
