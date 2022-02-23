6745b6b2f347ed0c2129cd61efd8b4f09b8471e1
Wibbler67 <76840856+Wibbler67@users.noreply.github.com>
2022-02-23 16:20:52 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,20 +2,20 @@
 # setup a mysql db in a docker container
 
 # install docker if it isn't already
-if ! docker --version > /dev/null; then
-    curl -V https://get.docker.com | bash
+if ! sudo docker --version > /dev/null; then
+    curl https://get.docker.com | sudo bash
 fi
 
 MYSQL_DATABASE="bookshelve"
 
 create_container() {
-    docker run -d \
+    sudo docker run -d \
         --name mysql \
         -p 3306:3306 \
         --health-cmd='mysqladmin ping --silent' \
         -e MYSQL_DATABASE="${MYSQL_DATABASE}" \
         -e MYSQL_ROOT_PASSWORD="${MYSQL_ROOT_PASSWORD}" \
-        -e MYSQL_USER="${MYSQL_USER}" \
+        -e MYSQL_USER="${MSQL_USER}" \
         -e MYSQL_PASSWORD="${MYSQL_PASSWORD}" \
         mysql:5.7
         while [[ "$(sudo docker inspect --format "{{ .State.Health.Status }}" mysql)" != "healthy" ]]; do 
@@ -27,21 +27,20 @@ create_container() {
 
 grant_user_read_access() {
     command="grant select on ${MYSQL_DATABASE}.* to '${MYSQL_USER}'@'%' identified by '${MYSQL_PASSWORD}'";
-    docker exec -i mysql mysql --connect-timeout=90 -uroot -p${MYSQL_ROOT_PASSWORD} -e  "${command}"
+    sudo docker exec -i mysql mysql --connect-timeout=90 -uroot -p${MYSQL_ROOT_PASSWORD} -e  "${command}"
 }
 
 run_sql_scripts() {
-    docker exec -i mysql mysql bookshelve \
+    sudo docker exec -i mysql mysql bookshelve \
         -uroot -p${MYSQL_ROOT_PASSWORD} < setup.sql 
 }
 
 # if the container doesn't exist
 if [ -z "$(sudo docker ps -qa -f name=mysql)" ]; then
-    echo "creating container"
     create_container
     grant_user_read_access
 # if the container is stopped
 elif [ -n "$(sudo docker ps -q -f status=exited -f name=mysql)" ]; then
-    docker start mysql
+    sudo docker start mysql
 fi
 run_sql_scripts
